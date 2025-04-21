f5bb986bef988c772adf45985310da045e0ce8b1
shebo77 <sheboahmed188@gmail.com>
2025-04-21 21:54:28 +0200

    forth commit

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -6,160 +6,137 @@ import { emailFunc } from "../../../services/sendEmail.js";
 import  {nanoid}  from "nanoid";
 
 
-export const signUp = asyncHandler(async(req , res , next) => {
-  const {name , email , password , address , phoneNumber , age} = req.body
-  const exist = await userModel.findOne({email : email.toLowerCase()})
-  if(exist){
-    return next(new AppError("email already exist" , 409))
+// ************************************signUp*************************************
+export const signUp = asyncHandler(async (req, res, next) => {
+  const { name, email, password, phone, address } = req.body
+
+  const emailExist = await userModel.findOne({ email: email.toLowerCase() })
+  if (emailExist) {
+      return next(new AppError("user already exist", 409))
   }
-  const token = jwt.sign({email} ,process.env.SIGNATURE , {expiresIn : 60*2} )
-  const link = `https://ecommerce-eight-roan-46.vercel.app/auth/confirmEmail/${token}`;
+  const token = jwt.sign({ email }, process.env.SIGNATURE, { expiresIn: 60 * 2 })
+  const link = `${req.protocol}://${req.headers.host}/auth/confirmEmail/${token}`
   const rfToken = jwt.sign({ email }, process.env.SIGNATURE)
-  const rfLink = `https://ecommerce-eight-roan-46.vercel.app/auth/confirmEmail/${rfToken}`;
+  const rfLink = `${req.protocol}://${req.headers.host}/auth/refreshToken/${rfToken}`
   const emailSend = emailFunc({
-    email,
-    subject: "confirm email",
-    html: `<a href='${link}'>confirm email</a> <br><a href='${rfLink}'>resend confirmEmail</a>`
-  });
-  
-    if(!emailSend){
-      return next(new AppError("failed to send this email" , 400))
-    }
-
-const hash = bcrypt.hashSync(password , +process.env.SaltRounds)
-const user = await userModel.create( {name , email , password : hash , address , phoneNumber , age})
-user ? res.status(201).json({msg : "done" , user}) : next(new AppError("fail" , 500))
-})
-
-
-
-
-export const confirmEmail = asyncHandler(async (req, res, next) => {
-  const { token } = req.params;
-
-  let decoded;
-  try {
-    decoded = jwt.verify(token, process.env.SIGNATURE); 
-  } catch (error) {
-    return next(new AppError("invalid token", 404)); 
-  }
-
-  if (!decoded?.email) {
-    return next(new AppError("invalid token", 404)); 
-  }
-
-  const user = await userModel.findOneAndUpdate(
-    { email: decoded.email, confirmed: false },
-    { confirmed: true },
-    { new: true }
-  );
-
-  if (!user) {
-    return next(new AppError("user not exist or already confirmed", 400));
-  }
-
-  res.status(200).json({ msg: "done" }); 
-});
-
-
-
-
-export const refreshToken = asyncHandler(async (req, res, next) => {
-  const { token } = req.params;
-
-  let decoded;
-  try {
-    decoded = jwt.verify(token, process.env.SIGNATURE);
-  } catch (error) {
-    return next(new AppError("invalid token", 400));
-  }
-
-  if (!decoded?.email) {
-    return next(new AppError("invalid token", 400));
-  }
-
-  const user = await userModel.findOne({ email: decoded.email, confirmed: false });
-  if (!user) {
-    return next(new AppError("user not exist or already confirmed", 400));
-  }
-
-  const rfToken = jwt.sign({ email: user.email }, process.env.SIGNATURE, { expiresIn: '2m' });
-  const link = `${req.protocol}://${req.headers.host}/auth/confirmEmail/${rfToken}`;
-
-  const emailSend = emailFunc({
-    email: user.email,
-    subject: "confirm email",
-    html: `<a href='${link}'>confirm email</a>`
-  });
-
-  if (!emailSend) {
-    return next(new AppError("failed to send this email", 400));
-  }
-
-  res.status(201).json({ msg: "done" });
-});
-
-
-
-
-
-export const forgetPassword = asyncHandler(async (req , res , next) => {
-  const {email} = req.body
-  const user = await userModel.findOne({email})
-  if(!user){
-    return next(new AppError("user not exist" ,400))
-
-  }
-
-  const code = nanoid(5)
-  const emailSend = emailFunc({
-    email , 
-    subject : "reset password",
-    html : `<h1>code : ${code}</h1>`
+      email,
+      subject: "confirm email",
+      html: `<a href='${link}'>confirm email</a> <br>
+      <a href='${rfLink}'>resend confirmEmail</a>`
   })
   if (!emailSend) {
-    return next(new AppError("fail to send this email", 400))
+      return next(new AppError("fail to send this email", 400))
 
-}
-  await userModel.updateOne({email} , {forgetCode : code})
-  res.status(200).json({msg : "done"})
+  }
+  const hash = bcrypt.hashSync(password, +process.env.SALT_ROUND)
+  const user = await userModel.create({ name, email, password: hash, phone, address })
+
+  user ? res.status(201).json({ msg: "done", user }) : next(new AppError("fail", 500))
 
 })
 
 
-export const resetPassword = asyncHandler(async (req , res , next) => {
-  const {email , code , newPassword , rePassword} = req.body
-  const user = await userModel.findOne({email})
-  if(!user){
-    return next(new AppError("user not exist" , 404))
+// ************************************confirmEmail*************************************
+export const confirmEmail = asyncHandler(async (req, res, next) => {
+  const { token } = req.params
+  const decoded = jwt.verify(token, process.env.SIGNATURE)
+  if (!decoded?.email) {
+      return next(new AppError("invalid token", 400))
+  }
+  const user = await userModel.findOneAndUpdate({ email: decoded.email, confirmed: false }, { confirmed: true }, { new: true })
+
+  user ? res.status(201).json({ msg: "done" }) : next(new AppError("user not exist or already  confirmed plz log in", 400))
+
+})
+
+
+// ************************************refreshToken*************************************
+export const refreshToken = asyncHandler(async (req, res, next) => {
+  const { token } = req.params
+  const decoded = jwt.verify(token, process.env.SIGNATURE)
+  if (!decoded?.email) {
+      return next(new AppError("invalid token", 400))
+  }
+  const user = await userModel.findOne({ email: decoded.email, confirmed: false })
+  if (!user) {
+      return next(new AppError("user not exist or already  confirmed plz log in", 400))
+  }
+  const rfToken = jwt.sign({ email: user.email }, process.env.SIGNATURE, { expiresIn: 60 * 2 })
+  const link = `${req.protocol}://${req.headers.host}/auth/confirmEmail/${rfToken}`
+  const emailSend = emailFunc({
+      email: user.email,
+      subject: "confirm email",
+      html: `<a href='${link}'>confirm email</a> `
+  })
+  if (!emailSend) {
+      return next(new AppError("fail to send this email", 400))
 
   }
-  if(user.forgetCode!== code){
-    return next(new AppError("invalid code" , 400))
+  res.status(201).json({ msg: "done" })
+
+})
+
+
+// ************************************forgetPassword*************************************
+export const forgetPassword = asyncHandler(async (req, res, next) => {
+  const { email } = req.body
+
+  const userExist = await userModel.findOne({ email })
+  if (!userExist) {
+      return next(new AppError("email not exist ", 404))
   }
-  const hash = bcrypt.hashSync(newPassword , +process.env.SaltRounds)
-  await userModel.updateOne({email} , {password : hash , forgetCode : "" , changePasswordAt : Date.now()})
-  res.status(200).json({msg : "done"})
+  const code = nanoid(4)
+  const emailSend = emailFunc({
+      email,
+      subject: "reset password",
+      html: `<h1>code:${code}</h1>`
+  })
+  if (!emailSend) {
+      return next(new AppError("fail to send this email", 400))
+  }
+  await userModel.updateOne({ email }, { forgetCode: code })
+  res.status(200).json({ msg: "done" })
+
 })
 
 
 
+// ************************************resetPassword*************************************
+export const resetPassword = asyncHandler(async (req, res, next) => {
+  const { email, code, password, rePassword } = req.body
+
+  const userExist = await userModel.findOne({ email })
+  if (!userExist) {
+      return next(new AppError("email not exist ", 404))
+  }
+  if (userExist.forgetCode !== code) {
+      return next(new AppError("invalid code ", 400))
+
+  }
+  const hash = bcrypt.hashSync(password, +process.env.SALT_ROUND)
+
+  await userModel.updateOne({ email }, { password: hash, forgetCode: "", changePasswordAt: Date.now() })
+
+  res.status(200).json({ msg: "done" })
+
+})
 
 
-export const signIn = asyncHandler(async (req , res ,next) => {
-  const {email , password} = req.body
-  const user = await userModel.findOne({email})
-  if(!user){
-    return next(new AppError("user not exist" , 404))
+// ************************************signIn*************************************
+export const signIn = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body
+
+  const userExist = await userModel.findOne({ email, confirmed: true })
+  if (!userExist) {
+      return next(new AppError("email not exist or not confirmed yet!! ", 404))
   }
-  if(user.confirmed == false){
-    return next(new AppError("user not confirmed" , 404))
+  const match = bcrypt.compareSync(password, userExist.password)
+  if (!match) {
+      return next(new AppError("password not match!! ", 400))
   }
-  const match = bcrypt.compareSync(password , user.password)
-  if(!match){
-    return next(new AppError("password not match" , 400))
-  }
-const token = jwt.sign({id : user._id , email , role : user.role} ,process.env.SIGNATURE )
-  await userModel.updateOne({email} , {loggedIn : true})
-  return res.status(200).json({msg : "done" , token})
+  const token = jwt.sign({ id: userExist._id, email, role: userExist.role }, process.env.SIGNATURE)
+  await userModel.updateOne({ email }, { loggedIn: true })
+
+  res.status(200).json({ msg: "done", token })
+
 })
--- src/modules/auth/auth.routes.js
+++ src/modules/auth/auth.routes.js
@@ -5,12 +5,13 @@ import* as AV from "./auth.validation.js";
 
 const authRouter = Router()
 
-authRouter.post("/signUp",validation(AV.signUp) , AC.signUp)
+
+authRouter.post("/signUp", validation(AV.signUp), AC.signUp)
 authRouter.get("/confirmEmail/:token", validation(AV.confirmEmail), AC.confirmEmail)
 authRouter.get("/refreshToken/:token", validation(AV.confirmEmail), AC.refreshToken)
-authRouter.patch("/sendCode" , validation(AV.forgetPassword) , AC.forgetPassword)
-authRouter.patch("/resetPassword" , validation(AV.resetPassword) , AC.resetPassword)
-authRouter.post("/signIn" , validation(AV.signIn) , AC.signIn)
+authRouter.patch("/sendCode", validation(AV.forgetPassword), AC.forgetPassword)
+authRouter.patch("/resetPassword", validation(AV.resetPassword), AC.resetPassword)
+authRouter.post("/signIn", validation(AV.signIn), AC.signIn)
 
 
 
